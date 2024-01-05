73f9050ac34023723050bf693fef46326fe3e47f
Gustavo Luis Schmaedecke <97gustavo.luis@gmail.com>
2024-01-04 23:57:14 -0300

    Criaçao do projeto, do controller e das Views

---
+++ MeuSiteEmMVC/Controllers/ContatoController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeuSiteEmMVC.Controllers {
+    public class ContatoController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ MeuSiteEmMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace MeuSiteEmMVC.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
