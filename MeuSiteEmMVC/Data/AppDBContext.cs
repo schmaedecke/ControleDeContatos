f6e9420464decca271b4e18846a53706180250b5
Gustavo Luis Schmaedecke <97gustavo.luis@gmail.com>
2024-01-06 17:15:53 -0300

    Tentativas de concectar ao DB

--- MeuSiteEmMVC/Data/AppDBContext.cs
+++ MeuSiteEmMVC/Data/AppDBContext.cs
@@ -1,11 +1,13 @@
 ﻿
 
+using MeuSiteEmMVC.Models;
+
 namespace MeuSiteEmMVC.Data {
     public class AppDBContext : DbContext{
         public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) {
         
         }
-        
+        public DbSet<ContatoModel> Contatos { get; set; }
 
     }
 }
